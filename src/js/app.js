d1626409c98b0c6eaf4ad7243569f024c934cfcd
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-12-04 22:57:14 +0500

    Added UI code

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@ App = {
   web3Provider: null,
   contracts: {},
 
-  init: async function() {
+  init: async () => {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
+    $.getJSON('../pets.json', function (data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
+      for (i = 0; i < data.length; i++) {
         petTemplate.find('.panel-title').text(data[i].name);
         petTemplate.find('img').attr('src', data[i].picture);
         petTemplate.find('.pet-breed').text(data[i].breed);
@@ -23,40 +23,97 @@ App = {
     return await App.initWeb3();
   },
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+  initWeb3: async () => {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access");
+      }
+    }
 
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+
+    web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+  initContract: () => {
+    $.getJSON('Adoption.json', (data) => {
+        // Get the necessary contract artifact file and instantiate it with @truffle/contract
+        var AdoptionArtifact = data;
+        App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+        // Set the provider for our contract
+        App.contracts.Adoption.setProvider(App.web3Provider);
+
+        // Use our contract to retrieve and mark the adopted pets
+        return App.markAdopted();
+      });
 
     return App.bindEvents();
   },
 
-  bindEvents: function() {
+  bindEvents: () => {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  markAdopted: function() {
-    /*
-     * Replace me...
-     */
+  markAdopted: () => {
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then((instance) => {
+      adoptionInstance = instance;
+
+      return adoptionInstance.getAdopters.call();
+    }).then((adopters) => {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch((err) => {
+      console.log(err.message);
+    });
+
   },
 
-  handleAdopt: function(event) {
+  handleAdopt: (event) => {
     event.preventDefault();
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    web3.eth.getAccounts((error, accounts) => {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then((instance) => {
+        adoptionInstance = instance;
+
+        // Execute adopt as a transaction by sending account
+        return adoptionInstance.adopt(petId, { from: account });
+      }).then((result) => App.markAdopted()
+      ).catch((err) => {
+        console.log(err.message);
+      });
+    });
+
   }
 
 };
